d6717bcfc14eb49029ef460a1774cb36ade517ad
rcintron1 <rolando.cintron@gmail.com>
2021-04-08 08:35:16 -0400

    first commit

--- index.js
+++ index.js
@@ -1,46 +1,24 @@
-const AWS = require("aws-sdk")
-const ec2 = new AWS.EC2({region:"us-east-1"});
-
-/**
- * 
- * @param {array} status An array with one or more of the following options:
- * creating | available | in-use | deleting | deleted | error
- */
-async function listVolumes(status){
-    var params = {
-        Filters: [
-            {
-              Name: 'status',
-              Values: status
-            },
-          ]
-    };
-    const volumes = (await ec2.describeVolumes(params).promise()).Volumes
-    
-    return volumes
-}
-
-/**
- * Delete volumes
- * @param {array} volumes 
- */
-async function deleteVolumes(volumes){
-
-}
+// const AWS = require("aws-sdk")
+// const ec2 = new AWS.EC2({region:"us-east-1"});
+import { listVolumes, deleteVolumes} from "./libs/ec2.js"
 
 const volumeStatus = ["available"]
 
 listVolumes(volumeStatus).then(volumes=>{
-    const result = []
-    console.log(`Count of available ${volumes.length}`)
-    volumes.forEach(async volume=>{
-        
-        try{
-            result.push (await ec2.deleteVolume({VolumeId: volume.VolumeId, DryRun:false}).promise())
-        }catch (e) {console.log(e)}
-        
-    })
-    Promise.all(result).then(values=>{
-        console.log(values.length>0?"failed":`successfully deleted ${volumes.length} available volumes`)
-    })
+    console.log(JSON.stringify(volumes))
 })
+
+// listVolumes(volumeStatus).then(volumes=>{
+//     const result = []
+//     console.log(`Count of available ${volumes.length}`)
+//     volumes.forEach(async volume=>{
+        
+//         try{
+//             result.push (await ec2.deleteVolume({VolumeId: volume.VolumeId, DryRun:false}).promise())
+//         }catch (e) {console.log(e)}
+        
+//     })
+//     Promise.all(result).then(values=>{
+//         console.log(values.length>0?"failed":`successfully deleted ${volumes.length} available volumes`)
+//     })
+// })
---
+++ libs/ec2.js
@@ -0,0 +1,37 @@
+import AWS from 'aws-sdk';
+const ec2 = new AWS.EC2({region:"us-east-1"});
+
+/**
+ * Delete volumes
+ * @param {array} volumes 
+ */
+async function deleteVolumes(volumes){
+    for (i=0; i<volumes.length; i++){
+        try{
+            result.push (await ec2.deleteVolume({VolumeId: volume.VolumeId, DryRun:false}).promise())
+        }catch (e) {console.log(e)} 
+    }
+
+}
+
+/**
+ * Returns volumes that matches the 
+ * @param {array} status An array with one or more of the following options:
+ * creating | available | in-use | deleting | deleted | error
+ */
+
+async function listVolumes(status){
+    var params = {
+        Filters: [
+            {
+              Name: 'status',
+              Values: status
+            },
+          ]
+    };
+    const volumes = (await ec2.describeVolumes(params).promise()).Volumes
+    
+    return volumes
+}
+
+export {deleteVolumes, listVolumes}
